b3a2e0dce3947ddb0ea6b92272bf41347bb0cd6e
sevgilisedat <sedatsevgili@gmail.com>
2017-05-21 01:39:48 +0300

    made main screen scrolled

--- screens/Main.js
+++ screens/Main.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { List, ListItem, Text } from 'native-base';
 
 export default class Main extends React.Component {
-
-
     render() {
         var items = [
-            'Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can'
+            'Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can',
+            'Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can',
+            'Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can',
+            'Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can',
         ];
         return (
             <List dataArray={items}
